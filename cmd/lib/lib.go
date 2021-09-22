15049b6e9049b2e598dc5a1084eb213df925f5d3
huoyinghui <yhhuo@yunjinginc.com>
2021-09-22 18:30:37 +0800

    add: +/-

---
+++ cmd/lib/lib.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"syscall/js"
+)
+
+func sayHi(value js.Value, args []js.Value) interface{} {
+	fmt.Println("Hi!")
+	fmt.Printf("value:%v\n", value)
+	fmt.Printf("args:%v\n", args)
+	return nil
+}
+
+func sayHello(value js.Value, args []js.Value) interface{} {
+	fmt.Println("sayHello!")
+	fmt.Printf("value:%v\n", value)
+	fmt.Printf("args:%v\n", args)
+	callback := args[0].String()
+	f := js.Global().Get(callback)
+	message := "sssss"
+	// 调用函数
+	f.Invoke(message)
+	//callback.Invoke(js.Null(), "Did you say "+message)
+	return nil
+}
+
+func callBack(value js.Value, args []js.Value) interface{} {
+	fmt.Println("callback!")
+	fmt.Printf("value:%v\n", value)
+	fmt.Printf("args:%v\n", args)
+	// 函数名字
+	name := args[0].String()
+	f := js.Global().Get(name)
+	// 调用函数
+	f.Invoke(args[1])
+	return nil
+}
+
+func add(value js.Value, args []js.Value) interface{}  {
+	if len(args) != 2 {
+		return nil
+	}
+	js.Global().Set("output", js.ValueOf(args[0].Int()+args[1].Int()))
+	fmt.Printf("ret:%v\n", js.ValueOf(args[0].Int() + args[1].Int()).String())
+	return nil
+}
+
+func subtract(value js.Value, args []js.Value) interface{} {
+	if len(args) != 2 {
+		return nil
+	}
+	js.Global().Set("output", js.ValueOf(args[0].Int()-args[1].Int()))
+	fmt.Printf("ret:%v\n", js.ValueOf(args[0].Int() -args[1].Int()).String())
+	return nil
+}
--- cmd/lib/main.go
+++ cmd/lib/main.go
@@ -1,45 +1,15 @@
 package main
+
 import (
-	"fmt"
 	"syscall/js"
 )
-
-func sayHi(value js.Value, args []js.Value) interface{} {
-	fmt.Println("Hi!")
-	fmt.Printf("value:%v\n", value)
-	fmt.Printf("args:%v\n", args)
-	return nil
-}
-
-func sayHello(value js.Value, args []js.Value) interface{} {
-	fmt.Println("sayHello!")
-	fmt.Printf("value:%v\n", value)
-	fmt.Printf("args:%v\n", args)
-	callback := args[0].String()
-	f := js.Global().Get(callback)
-	message := "sssss"
-	// 调用函数
-	f.Invoke(message)
-	//callback.Invoke(js.Null(), "Did you say "+message)
-	return nil
-}
-
-func callBack(value js.Value, args []js.Value) interface{} {
-	fmt.Println("callback!")
-	fmt.Printf("value:%v\n", value)
-	fmt.Printf("args:%v\n", args)
-	// 函数名字
-	name := args[0].String()
-	f := js.Global().Get(name)
-	// 调用函数
-	f.Invoke(args[1])
-	return nil
-}
 
 func registerCallbacks() {
 	js.Global().Set("sayHi", js.FuncOf(sayHi))
 	js.Global().Set("sayHello", js.FuncOf(sayHello))
 	js.Global().Set("callBack", js.FuncOf(callBack))
+	js.Global().Set("add", js.FuncOf(add))
+	js.Global().Set("subtract", js.FuncOf(subtract))
 }
 
 func main() {
@@ -48,3 +18,5 @@ func main() {
 	<-c
 }
 // GOARCH=wasm GOOS=js go build -o assets/lib.wasm cmd/lib/main.go
+// cd cmd/lib
+// GOARCH=wasm GOOS=js go build -o ../../assets/lib.wasm
