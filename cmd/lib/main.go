d3d1a14a9f6bc3be1e5d9241fd8eae22a57889ad
huoyinghui <yhhuo@yunjinginc.com>
2021-09-22 18:11:45 +0800

    add: cb

--- cmd/lib/main.go
+++ cmd/lib/main.go
@@ -24,9 +24,22 @@ func sayHello(value js.Value, args []js.Value) interface{} {
 	return nil
 }
 
+func callBack(value js.Value, args []js.Value) interface{} {
+	fmt.Println("callback!")
+	fmt.Printf("value:%v\n", value)
+	fmt.Printf("args:%v\n", args)
+	// 函数名字
+	name := args[0].String()
+	f := js.Global().Get(name)
+	// 调用函数
+	f.Invoke(args[1])
+	return nil
+}
+
 func registerCallbacks() {
 	js.Global().Set("sayHi", js.FuncOf(sayHi))
 	js.Global().Set("sayHello", js.FuncOf(sayHello))
+	js.Global().Set("callBack", js.FuncOf(callBack))
 }
 
 func main() {
